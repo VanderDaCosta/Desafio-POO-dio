a5c9bfad2d169ac89f5adf9d23ee408c88df2d3d
Vander da Costa <opessoaldovander@gmail.com>
2023-01-20 16:19:32 -0300

    Quarta semana de estudos finalizada

--- ExercicioDeCalculo/src/Calculadora.java
+++ ExercicioDeCalculo/src/Calculadora.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Calculadora {
+/*
+ Classe de exemplo para atividade da Calculadora de Métodos
+*/
+public class Calculadora {
+    public static void soma(double numero1, double numero2) {
+        double resultado = numero1 + numero2;
+
+        System.out.println("A soma entre " + numero1 + " e " + numero2 + " é " + resultado);
+    }
+    public static void subtracao(double numero1, double numero2){
+        double resultado = numero1 - numero2;
+
+        System.out.println("A subtração entre " + numero1 + " e " + numero2 + " é " + resultado);
+    }
+    public static void multiplicacao(double numero1, double numero2){
+        double resultado = numero1 * numero2;
+
+        System.out.println("A multiplicacao entre " + numero1 + " e " + numero2 + " é " + resultado);
+    }
+    public static void divisao(double numero1, double numero2){
+        double resultado = numero1 / numero2;
+
+        System.out.println("A divisao entre " + numero1 + " e " + numero2 + " é " + resultado);
+    }
+
 }
--- ExercicioDeCalculo/src/Emprestimo.java
+++ ExercicioDeCalculo/src/Emprestimo.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Emprestimo {
+/*
+ Classe de exemplo para atividade da Empréstimo de Métodos
+*/
+public class Emprestimo {
+    public static double taxaParcelas() {
+    return 0.2;
+    }
+    public static void calcular (double valor, double qtdParcelas){
+        double resultado = valor + (valor * ( qtdParcelas * taxaParcelas()));
+        System.out.println("O valor final do emprestimo é " + resultado);
+    }
 }
