f4e48295621c375427da5e9b2987911dc2814666
Vander da Costa <opessoaldovander@gmail.com>
2023-02-10 17:06:45 -0300

    quinta e sexta semana de estudos de JAVA

---
+++ OOAula03/src/ClasseFilha1.java
@@ -0,0 +1,7 @@
+public class filha1 extends Mae  {
+    //override serve para sobreescrever
+    @Override
+    void metodo1(){
+        System.out.println("Metódo 1 da classe filha 1 ");
+    }
+}
---
+++ OOAula03/src/ClasseFilha2.java
@@ -0,0 +1,10 @@
+public class filha2 extends Mae {
+    @Override
+    void metodo1 (){
+        System.out.println("Metodo 1 da classe filha 2");
+    }
+    @Override
+    void metodo2 (){
+        System.out.println("Metodo 2 da classe filha 2");
+    }
+}
