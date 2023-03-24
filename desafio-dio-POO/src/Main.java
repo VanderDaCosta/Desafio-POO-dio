0f35079da0dc39ba30a90d44319d7ea48ac1558e
Vander da Costa <opessoaldovander@gmail.com>
2023-03-24 17:11:16 -0300

    atualização estudos java 24/03/2023

--- desafio-dio-POO/src/Main.java
+++ desafio-dio-POO/src/Main.java
@@ -1,6 +1,28 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso de Java");
+        curso1.setDescricao("Inicialização em java");
+        curso1.setCargoHoraria(8);
 
-        System.out.println("Hello world!");
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso de CSS");
+        curso2.setDescricao("Introdução ao CSS");
+        curso2.setCargoHoraria(6);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("mentoria de Java");
+        mentoria1.setDescricao("ajudando você com o java");
+        mentoria1.setData(LocalDate.now());
+        
+        System.out.println(mentoria1);
     }
 }
